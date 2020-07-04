dcea446376196b58090e8e6524b0b0cbbd89aac1
vit-vokhminov <46236101+vit-vokhminov@users.noreply.github.com>
2020-07-05 03:28:57 +0700

    test and devtools

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,15 +1,15 @@
-import {combineReducers, createStore} from "redux";
+import {combineReducers, compose, createStore} from "redux";
 import appReducer from "./app-reducer";
 
 let rootReducer = combineReducers({
     appReducer: appReducer,
 });
 
-type RootReducerType = typeof rootReducer;
+type RootReducerType = typeof rootReducer;  // (globalstate: AppStateType) => AppStateType
 export type AppStateType = ReturnType<RootReducerType>
 
-let store = createStore(rootReducer);
-
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer,  composeEnhancers());
 
 
 // @ts-ignore
